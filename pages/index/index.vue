0811e02040b82e58f6c80304efd9b198846206dd
qiuzhe <2810201146@qq.com>
2021-01-12 16:39:49 +0800

    添加logo

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,76 +1,80 @@
 <template>
+	
 	<view class="content">
-		<image class="logo" src=""></image>
+		<img class="img_index" src="../../static/zhcy_index.png" />
+		<!-- <image  src="../../static/5fe9ed6bba4e3d195c91b6634007d6c.png"></image> -->
+		<!-- <image  src="../../static/5fe9ed6bba4e3d195c91b6634007d6c.png" ></image> -->
 		<view class="text-area">
-			
-			<view class="title">  
-				<navigator url="/pages/knowing/floor" hover-class="navigator-hover">
-					晚查寝
-				</navigator>
-			</view>
-			
+			<view class="title"><navigator url="/pages/knowing/floor" hover-class="navigator-hover">晚查寝</navigator></view>
 			<view class="title" @click="msg()">晚自修</view>
 			<view class="title" @click="msg()">早签</view>
+			<view class="title" @click="msg()">请假</view>
 		</view>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-			
-		},
-		methods: {
-			msg(){
-				uni.showToast({
-					title:"功能开发中",
-					icon:"none"
-				})
-			}
+export default {
+	data() {
+		return {
+			title: 'Hello'
+		};
+	},
+	onLoad() {},
+	methods: {
+		msg() {
+			uni.showToast({
+				title: '功能开发中',
+				icon: 'none'
+			});
 		}
 	}
+};
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
+.content {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	padding-top: 150rpx;
+	justify-content: center;
+}
+.img_index {
+	width: 200px;
+	padding-bottom: 150rpx;
+	/* height: 200px; */
+}
+.logo {
+	height: 120rpx;
+	width: 200rpx;
+	margin-top: 200rpx;
+	margin-left: auto;
+	margin-right: auto;
+	margin-bottom: 50rpx;
+}
 
-	.logo {
-		height: 120rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
+.text-area {
+	display: flex;
+	/* flex-direction: column; */
+	flex-wrap: wrap;
+	width: 80%;
+	margin: 0 auto;
+	justify-content: center;
+	/* align-items: center; */
+}
 
-	.text-area {
-		display: flex;
-		/* flex-direction: column; */
-		justify-content: space-between;
-		align-items: center;
-		
-	}
+.title {
+	font-size: 36rpx;
+	text-align: center;
+	color: #ffffff;
+	width: 200rpx;
+	height: 200rpx;
+	line-height: 200rpx;
+	/* padding: 50rpx; */
+	border-radius: 50%;
+	margin: 20rpx;
+	background-color: #23c5ff;
+}
 
-	.title {
-		font-size: 36rpx;
-		text-align: center;
-		color: #FFFFFF;
-		width: 200rpx;
-		height: 200rpx;
-		line-height: 200rpx;
-		/* padding: 50rpx; */
-		border-radius: 50%;
-		margin: 20rpx;
-		background-color: #23c5ff;
-	}
 </style>
