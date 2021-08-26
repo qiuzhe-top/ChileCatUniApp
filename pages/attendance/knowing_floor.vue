9a53af2bb9b004b970ca1351bd7e739454225a1d
qiuzhe <2810201146@qq.com>
2021-08-26 11:03:01 +0800

    完成任务页面跳转 补页面代码

---
+++ pages/attendance/knowing_floor.vue
@@ -0,0 +1,11 @@
+<template>
+	<view class="">
+		晚查寝
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ pages/attendance/late.vue
@@ -0,0 +1,11 @@
+<template>
+	<view class="">
+		晚自修
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
